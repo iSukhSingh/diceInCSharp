af1481ba329bb12982289920c24c1aeeaaaaa087
cali1204 <qwertyuiop.s1204@gmail.com>
2022-12-10 21:42:58 -0500

    if statements added

--- program.cs
+++ program.cs
@@ -3,10 +3,24 @@
 namespace Dice_Game
 {
     class program
-    {
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("press any key to roll thje dice");
+            int playerRandomNum;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("press any key to roll thje dice");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                Console.WiteLine("you have rolled a " + playerRandomNum);
+            }
+						Console.readKey();
+           
         }
     }
 }
