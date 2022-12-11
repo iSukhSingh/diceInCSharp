6b118d0b1ca80cefb692e6df76a42f1ad5726988
cali1204 <qwertyuiop.s1204@gmail.com>
2022-12-10 21:46:40 -0500

    added the second player

--- program.cs
+++ program.cs
@@ -3,10 +3,11 @@
 namespace Dice_Game
 {
     class program
-    { 
+    {
         static void Main(string[] args)
         {
             int playerRandomNum;
+						int enemyRandomNum;
 
             Random random = new Random();
 
@@ -17,10 +18,18 @@ namespace Dice_Game
                 Console.ReadKey();
 
                 playerRandomNum = random.Next(1, 7);
-                Console.WiteLine("you have rolled a " + playerRandomNum);
+                Console.WriteLine("you have rolled a " + playerRandomNum);
+								System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine("AI have rolled a " + enemyRandomNum);
             }
-						Console.readKey();
-           
+
+
+
+
+            Console.ReadKey();
+
         }
     }
 }
